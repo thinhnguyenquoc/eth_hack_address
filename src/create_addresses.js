5d04e09fb96dd32ac688f950d3792cd00a987910
Thinh Nguyen <thinhnq@blockchainlabs.asia>
2018-10-29 15:29:00 +0700

    add web3

--- src/create_addresses.js
+++ src/create_addresses.js
@@ -1,13 +1,16 @@
 var seed 		= 'buzz guitar noodle chief aspect glory concert jewel craft mixed bicycle butter'
 const numAddrs	= 1;
 const ETC 		= require('./etc.js')
-
+const Web3      = require('web3')
 var etc = new ETC()
 var fs = require('fs');
 const axios = require('axios');
 const readLastLines = require('read-last-lines');
 const Common            = require('./common.js');
 var common = new Common();
+// Need to change IP and port to connect server
+var web3        = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/vzNvkEyCh7ynImzgkWOn"));
+var web3_2        = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/0BGbYJC2pHEUHcjm7nj8")); 
 
 readLastLines.read('keys.txt', 2)
     .then((lines) => r(parseInt(lines)));
@@ -25,7 +28,36 @@ var r = (i) =>{
 	setTimeout(function(){ 
 		var addr = etc.createWalletFromSeed(seed, null, i);
 		console.log('addr', addr.address);
-		if(i%3==0){
+		if(i%5==0){
+			web3.eth.getBalance(addr.address).then((data)=>{
+				console.log('web3_infura',data);
+				if(parseFloat(data) != 0){
+					fs.appendFile('keys.txt', i + " " + JSON.stringify(addr) + "\n\t", function (err) {
+						if (err) {					
+							r(i);
+						}
+						else{
+							console.log(i);
+							i++;
+							r(i);
+							console.log('Saved ok!', addr);
+						}					
+					});
+				}
+				else{
+					fs.appendFile('keys.txt', i + "\n\t", function (err) {
+						if (err) 
+							r(i);
+						else{
+							console.log(i);
+							i++;
+							r(i);
+						}
+					});
+				}
+			});
+		}
+		else if(i%5==1){
 			axios.get(`https://api.blockcypher.com/v1/eth/main/addrs/${addr.address}/balance`).then((data)=>{
 				console.log('blockcypher',data.data.balance);
 				if(parseFloat(data.data.balance) != 0){
@@ -54,7 +86,36 @@ var r = (i) =>{
 				}
 			});
 		}
-		else if(i%3 == 1){
+		else if(i%5==2){
+			web3_2.eth.getBalance(addr.address).then((data)=>{
+				console.log('web3_2_infura',data);
+				if(parseFloat(data) != 0){
+					fs.appendFile('keys.txt', i + " " + JSON.stringify(addr) + "\n\t", function (err) {
+						if (err) {					
+							r(i);
+						}
+						else{
+							console.log(i);
+							i++;
+							r(i);
+							console.log('Saved ok!', addr);
+						}					
+					});
+				}
+				else{
+					fs.appendFile('keys.txt', i + "\n\t", function (err) {
+						if (err) 
+							r(i);
+						else{
+							console.log(i);
+							i++;
+							r(i);
+						}
+					});
+				}
+			});
+		}
+		else if(i%5 == 3){
 			axios.get(`http://api.ethplorer.io/getAddressInfo/${addr.address}?apiKey=freekey`).then((data)=>{
 				console.log('ethplorer',data.data.ETH.balance);
 				if(parseFloat(data.data.ETH.balance) != 0){
@@ -113,5 +174,6 @@ var r = (i) =>{
 				}
 			});
 		}
-	}, 3000);
+	}, 2000);
+	
 }
